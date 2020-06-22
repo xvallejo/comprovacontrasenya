4a5b0638d0f557c8eab6a0aef67ce849dfd389de
Xavier Vallejo <xavier.vallejo@arinnova.com>
2020-06-22 16:39:57 +0200

    Versio 1 del codi

--- src/main/java/com/xvallejo/comprovacontrasenya/Main.java
+++ src/main/java/com/xvallejo/comprovacontrasenya/Main.java
@@ -11,4 +11,37 @@ package com.xvallejo.comprovacontrasenya;
  */
 public class Main {
 	
+	public static void main(String[] args) {
+
+		String caractersAdmesos = "abcdefghijklmnopqrstuvwxyzñç";
+		String numerosAdmesos = "1234567890";
+
+		String contrasenya = "aaaaa22aa";
+
+		if (contrasenya.length() < 8){
+			System.out.println("La contrasenya es massa curta");
+		}
+		else{
+			int algunaLletra = 0;
+			int algunNumero = 0;
+			for (int posicio=0;posicio<contrasenya.length();posicio++){
+				if (caractersAdmesos.indexOf(contrasenya.charAt(posicio))>=0){
+					algunaLletra = 1;
+				}
+				if (numerosAdmesos.indexOf(contrasenya.charAt(posicio))>=0){
+					algunNumero = 1;
+				}
+			}
+			if (algunaLletra == 0){
+				System.out.println("La contrasenya ha de contenir alguna lletra");
+			}
+			else if (algunNumero == 0){
+				System.out.println("La contrasenya ha de contenir algun numero");
+			}
+			else{
+				System.out.println("Contrasenya correcta");
+			}
+		}
+		
+	}
 }
